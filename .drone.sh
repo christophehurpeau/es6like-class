c77dd14f72ae062e2235a51fe1f9ca32259395f0
Christophe Hurpeau <christophe@hurpeau.com>
2015-03-25 01:42:08 +0100

    drone.sh

--- .drone.sh
+++ .drone.sh
@@ -5,13 +5,13 @@ npm install -g karma-cli
 npm install
 
 echo "\n> Ensure that the code is warning free"
-node_modules/.bin/gulp lint || exit 1
+make lint || exit 1
 
 echo "\n> Build"
-node_modules/.bin/gulp build-all || exit 1
+make build || exit 1
 
 echo "\n> Run tests for node"
-node_modules/.bin/mocha --harmony -u tdd tests/lib/ || exit 1
+make tests || exit 1
 
 #echo "\n> Prepare tests for browsers"
 #node_modules/.bin/gulp springbokjs-shim || exit 1
@@ -21,7 +21,7 @@ node_modules/.bin/mocha --harmony -u tdd tests/lib/ || exit 1
 #karma start karma.conf.js --single-run --browsers=Firefox,Chrome,PhantomJS || exit 1
 
 echo "\n> Generate docs (api and coverage)"
-node_modules/.bin/gulp docs || exit 1
+make docs || exit 1
 
 echo "\n> Copy docs up to github gh-pages branch"
 mv docs docs-tmp
